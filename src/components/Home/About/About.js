7c36a2084ea552f7b68aa60de91b463daf2e74e1
Eli <iamelimars@yahoo.com>
2019-01-18 19:19:07 -0500

    Added about section

---
+++ src/components/Home/About/About.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './About.scss';
+class About extends Component {
+    render() {
+        return (
+            <div className="about-container">
+                <h1>About<span>.</span></h1>
+                <p>An overview of myself.</p>
+            </div>
+        )
+    }
+}
+
+export default About
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,13 @@
-import React, {Component} from 'react'
-import Header from '../../components/Home/Header'
+import React, {Component} from 'react';
+import Header from '../../components/Home/Header';
+import About from '../../components/Home/About/About';
 
 class Home extends Component {
     render() {
         return (
                 <div>
                     <Header />
+                    <About />
                 </div>
         )
     }
