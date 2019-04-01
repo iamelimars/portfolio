37a094069594cae164c5e9c68b13a024f0ec3f77
Elisha Marshall <iamelimars@yahoo.com>
2019-04-01 15:17:05 -0400

    Posts reducer with tests

--- src/components/Home/Header/spec.js
+++ src/components/Home/Header/spec.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import Header from './index';
+// import React from 'react';
+// import {shallow} from 'enzyme';
+// import Header from './index';
 
 it('should render without errors', () => {
     expect(1).toBe(1);
