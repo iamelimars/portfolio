a09c23a226c2a9d064d064c6069e536ecb37defe
Elisha Marshall <iamelimars@yahoo.com>
2019-04-03 21:27:11 -0400

    Added header section to svg

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import Header from './Header';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                Home
+                <Header />
             </div>
         )
     }
